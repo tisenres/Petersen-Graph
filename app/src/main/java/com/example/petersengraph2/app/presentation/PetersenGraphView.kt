40af6f41a6a3296a9865afa8c9242f41e461b638
tisenres <nastywushu@gmail.com>
2023-11-14 21:37:40 +0500

    Complete Generalized Petersen Graph

--- app/src/main/java/com/example/petersengraph2/app/presentation/PetersenGraphView.kt
+++ app/src/main/java/com/example/petersengraph2/app/presentation/PetersenGraphView.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import com.example.petersengraph2.app.presentation.themes.Theme
 import java.lang.Math.cos
@@ -26,21 +27,91 @@ class PetersenGraphView @JvmOverloads constructor(
         isAntiAlias = true
     }
 
+//    override fun onDraw(canvas: Canvas) {
+//        super.onDraw(canvas)
+//
+//        val centerX = width / 2f
+//        val centerY = height / 2f
+//        val radius = centerX.coerceAtMost(centerY) - 10f
+//
+//        for ((start, end) in edgeSet) {
+//            val startX = centerX + radius * cos(2 * Math.PI * start / n).toFloat()
+//            val startY = centerY + radius * sin(2 * Math.PI * start / n).toFloat()
+//
+//            val endX = centerX + radius * cos(2 * Math.PI * end / n).toFloat()
+//            val endY = centerY + radius * sin(2 * Math.PI * end / n).toFloat()
+//
+//            canvas.drawLine(startX, startY, endX, endY, paint)
+//        }
+//    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        val outerPoints: MutableList<Pair<Float, Float>> = mutableListOf()
+        val innerPoints:MutableList<Pair<Float, Float>> = mutableListOf()
+
         val centerX = width / 2f
         val centerY = height / 2f
-        val radius = centerX.coerceAtMost(centerY) - 10f
+        val radius = minOf(centerX, centerY) - 20f // Ensure the graph fits within the view
 
-        for ((start, end) in edgeSet) {
-            val startX = centerX + radius * cos(2 * Math.PI * start / n).toFloat()
-            val startY = centerY + radius * sin(2 * Math.PI * start / n).toFloat()
+        // Draw outer vertices
+        for (i in verticesSet.indices) {
+            val angle = 2 * Math.PI * i / verticesSet.size
+            val x = centerX + radius * cos(angle).toFloat()
+            val y = centerY + radius * sin(angle).toFloat()
 
-            val endX = centerX + radius * cos(2 * Math.PI * end / n).toFloat()
-            val endY = centerY + radius * sin(2 * Math.PI * end / n).toFloat()
+            val point = Pair(x, y)
+            outerPoints.add(point)
 
-            canvas.drawLine(startX, startY, endX, endY, paint)
+            canvas.drawCircle(x, y, 10f, paint)
+        }
+
+        // Draw inner vertices
+        for (i in verticesSet.indices) {
+            val angle = 2 * Math.PI * i / verticesSet.size
+            val x = centerX + radius * cos(angle).toFloat() / 2
+            val y = centerY + radius * sin(angle).toFloat() / 2
+
+            val point = Pair(x, y)
+            innerPoints.add(point)
+
+            canvas.drawCircle(x, y, 10f, paint)
+        }
+
+        // Draw inner edges
+        for (i in verticesSet.indices) {
+            val x1 = centerX + radius * cos(2 * Math.PI * i / verticesSet.size).toFloat() / 2
+            val y1 = centerY + radius * sin(2 * Math.PI * i / verticesSet.size).toFloat() / 2
+
+            val x2 = centerX + radius * cos(2 * Math.PI * (i + k) / verticesSet.size).toFloat() / 2
+            val y2 = centerY + radius * sin(2 * Math.PI * (i + k) / verticesSet.size).toFloat() / 2
+
+            canvas.drawLine(x1, y1, x2, y2, paint)
+        }
+
+        // Draw outer edges
+        for (i in verticesSet.indices) {
+
+            val x1 = outerPoints[i].first
+            val y1 = outerPoints[i].second
+
+            val x2 = outerPoints[(i+1) % verticesSet.size].first
+            val y2 = outerPoints[(i+1) % verticesSet.size].second
+
+            canvas.drawLine(x1, y1, x2, y2, paint)
+        }
+
+        // Draw edges between inner and outer edges
+        for (i in verticesSet.indices) {
+
+            val x1 = innerPoints[i].first
+            val y1 = innerPoints[i].second
+
+            val x2 = outerPoints[i].first
+            val y2 = outerPoints[i].second
+
+            canvas.drawLine(x1, y1, x2, y2, paint)
         }
     }
 
